6465896d918d6a4ef094f9ec0150d085f68ec4e9
Raffael Eloi <raffaeleloi121@gmail.com>
2026-05-15 05:44:15 -0300

    create infrastructure layer

---
+++ src/Timezone.Management.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Timezone.Management.Application.Contracts.Repositories;
+using Timezone.Management.Application.Entities;
+
+namespace Timezone.Management.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    public Task<User> AddUser(User user) =>
+        throw new NotImplementedException();
+
+    public Task DeleteUser(Guid userUid) =>
+        throw new NotImplementedException();
+
+    public Task<User?> GetUserByUid(Guid userUid) =>
+        throw new NotImplementedException();
+
+    public Task UpdateUser(Guid userUid, User user) =>
+        throw new NotImplementedException();
+}
