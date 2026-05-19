c6fa8c0bbb85977e7372f8ceddc75e6d0b2984b0
Raffael Eloi <raffaeleloi121@gmail.com>
2026-05-19 14:04:57 -0300

    add imports.tf temporarily

---
+++ infrastructure/imports.tf
@@ -0,0 +1,4 @@
+import {
+  to = azurerm_app_configuration.appconf
+  id = "/subscriptions/${var.subscription_id}/resourceGroups/RaffaLabRG/providers/Microsoft.AppConfiguration/configurationStores/timezone-management-app-config"
+}
