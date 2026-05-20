3acac24877d3c3718184f708a034c98cc62ff56e
Raffael Eloi <raffaeleloi121@gmail.com>
2026-05-19 21:29:58 -0300

    remove imports.tf file

--- infrastructure/imports.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = azurerm_app_configuration.appconf
-  id = "/subscriptions/${var.subscription_id}/resourceGroups/RaffaLabRG/providers/Microsoft.AppConfiguration/configurationStores/timezone-management-app-config"
-}
