4295c84d7591bb0e6a41abeff20646a075a53b5f
Raffael Eloi <raffaeleloi121@gmail.com>
2026-05-20 13:16:14 -0300

    allow azure services to access postgres db

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -217,3 +217,10 @@ resource "azurerm_postgresql_flexible_server_database" "timezonemanagementdb" {
     prevent_destroy = true
   }
 }
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_azure_services" {
+  name             = "allow-azure-services"
+  server_id        = azurerm_postgresql_flexible_server.timezonemanagementserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
