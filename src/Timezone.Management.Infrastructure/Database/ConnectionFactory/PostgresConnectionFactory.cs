afe21bb187b9bb20ba50da6dcfe08e870ac3be71
Raffael Eloi <raffaeleloi121@gmail.com>
2026-05-26 18:46:04 -0300

    user primary constructor on PostgresConnectionFactory

--- src/Timezone.Management.Infrastructure/Database/ConnectionFactory/PostgresConnectionFactory.cs
+++ src/Timezone.Management.Infrastructure/Database/ConnectionFactory/PostgresConnectionFactory.cs
@@ -5,17 +5,9 @@ using Timezone.Management.Infrastructure.Database.Contracts;
 
 namespace Timezone.Management.Infrastructure.Database.ConnectionFactory;
 
-public class PostgresConnectionFactory : IDbConnectionFactory
+public class PostgresConnectionFactory(IConfiguration configuration) : IDbConnectionFactory
 {
-	private readonly string _connectionString;
-#pragma warning disable IDE0021 // Use expression body for constructor
-#pragma warning disable IDE0290 // Use primary constructor
-	public PostgresConnectionFactory(IConfiguration configuration)
-#pragma warning restore IDE0290 // Use primary constructor
-	{
-		_connectionString = configuration.GetConnectionString("Postgres")!;
-	}
-#pragma warning restore IDE0021 // Use expression body for constructor
+	private readonly string _connectionString = configuration.GetConnectionString("Postgres")!;
 
 	public IDbConnection CreateConnection() => new NpgsqlConnection(_connectionString);
 }
